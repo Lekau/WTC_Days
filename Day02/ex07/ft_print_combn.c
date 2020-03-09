3aa0bb89683f8b4f3545162fdc7f76ec3173911d
Lekau Mamabolo <lekau9222@gmail.com>
2020-03-09 14:19:53 +0200

    ft_print_combn using nested while loops

--- Day02/ex07/ft_print_combn.c
+++ Day02/ex07/ft_print_combn.c
@@ -16,9 +16,41 @@ void ft_print_combn(int n)
     char g;
     char h;
     char i;
-    char j;
-    while( n == 1 && a <= '9')
+
+    while( n <= 1 && a <= '9')
     {
+        while (n <= 2 && b <= '9')
+        {
+            while (n <= 3 && c <= '9')
+            {
+                while (n <= 4 && d <= '9')
+                {
+                    while (n <= 5 && e <= '9')
+                    {
+                        while (n <= 6 && f <= '9')
+                        {
+                            while (n <= 7 && g <= '9')
+                            {
+                                while (n <= 8 && h <= '9')
+                                {
+                                    while (n <= 9 && i <= '9')
+                                    {
+                                        i++;
+                                    }
+                                    h++;
+                                }
+                                g++;
+                            }
+                            f++;
+                        }
+                        e++;
+                    }
+                    d++;
+                }
+                c++;
+            }
+            b++;
+        }
         a++;
     }
 }
