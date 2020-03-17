4e65d958826b9ce67f9f660ac85bc520d49ae867
Lekau Mamabolo <lekau9222@gmail.com>
2020-03-17 19:51:01 +0200

    recreation of the C function atoi, done and TESTED

--- Day03/ex08/ft_atoi.c
+++ Day03/ex08/ft_atoi.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 int ft_atoi(char *str)
 {
     int kau;
@@ -5,18 +6,29 @@ int ft_atoi(char *str)
     int is_neg;
     kau = 0;
     is_neg = 1;
-
-    while(!(str[i] >= '0' && str[i] <= '9') && str[i] != '-')
+    i = 0;
+    while(((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')) && str[i] != '-')
         i++;
+
     if (str[i] == '-')
     {
         is_neg = -1;
         i++;
     }
+
     while(str[i] >= '0' && str[i] <= '9')
     {
-        kau = (kau * 10)+(str[i] - '0');
+        kau = (kau * 10) + (str[i] - '0');
         i++;
     }
+
     return (kau * is_neg);
+}
+int main()
+{
+    char *num;
+
+    num = "dgf-63lekau";
+    printf("%d", ft_atoi(num));
+    return(0);
 }
