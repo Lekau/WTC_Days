1ea6fba94ce6c31aa3614eb12804c5a05f99276e
Lekau Mamabolo <lekau9222@gmail.com>
2020-03-19 00:34:42 +0200

    Seg Fault, only works with malloc, hmmm

--- Day03/ex07/ft_strrev.c
+++ Day03/ex07/ft_strrev.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+
 char *ft_strrev(char *str)
 {
     int i;
     int k; 
-    char* temp;
+    char *temp;
     i = 0;
     k = 0;
-    temp = str;
     while (str[k] != '\0')
     {
         k++;
     }
+    temp = str;
     k--;
     while (k >= 0)
     {
@@ -18,7 +19,7 @@ char *ft_strrev(char *str)
         i++;
         k--;
     }
-    str[i] = '\0';
+    temp[i] = '\0';
     return(str);
 }
 
