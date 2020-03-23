e6b90438db74f989d4f411e0df19d2abbe190645
Lekau Mamabolo <lekau9222@gmail.com>
2020-03-23 22:10:30 +0200

    all done, left testing code in there...

--- Day03/ex09/ft_sort_integer_table.c
+++ Day03/ex09/ft_sort_integer_table.c
@@ -1,1 +1,60 @@
-HEllo World
+#include <unistd.h>
+char ft_putnbr(int nbr)
+{
+    char cnbr;
+
+    cnbr = nbr + '0';
+
+    return (cnbr);
+}
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void ft_sort_integer_table(int *tab, int size)
+{
+    int sorted[size];
+    int i; 
+    int k;
+    int tmp;
+
+    k = 0;
+    i = 0;
+    while (i < size)
+    {
+        sorted[i] = tab[i];
+        i++;
+    }
+    while(k < size)
+    {
+        i = 0;
+        while(i < size)
+        {
+            if (sorted[k] < sorted[i])
+            {
+                tmp = sorted[k];
+                sorted[k] = sorted[i];
+                sorted[i] = tmp;
+            }
+            i++;
+        }
+        k++;
+    }
+    //testing 
+    int s = 0;
+    while(s < size)
+    {
+        ft_putchar(ft_putnbr(sorted[s]));
+        s++;
+    }
+}
+
+int main ()
+{
+    int tab[5] = {4,2,5,1,3};
+    int size = 5;
+    ft_sort_integer_table(tab, size);
+    return (0);
+}
