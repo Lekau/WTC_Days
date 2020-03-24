5eaade192ef5e17b613a162fc03772e2418eac2f
Lekau Mamabolo <lekau9222@gmail.com>
2020-03-24 19:03:49 +0200

    added some error handling for 0 and neg nums

--- Day04/ex00/ft_iterative_factorial.c
+++ Day04/ex00/ft_iterative_factorial.c
@@ -1,12 +1,23 @@
 int ft_iterative_factorial(int nb)
 {
-    int i;
-    i = nb - 1;
-
-    while (i > 0)
+    if (nb > 0)
     {
-        nb = nb * i;
-        i--;
+        int i;
+        i = nb - 1;
+
+        while (i > 0)
+        {
+            nb = nb * i;
+            i--;
+        }
+        return (nb);
     }
-    return (nb);
+    else if (nb == 0)
+    {
+        return (1);
+    }
+    else 
+    {
+        return (0);
+    }
 }
